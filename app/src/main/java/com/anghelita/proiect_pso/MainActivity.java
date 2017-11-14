6e36128b7d8b6de9a85835723104644be4063f9c
Alex Ciobanu <alex.ciobanu.a@gmail.com>
2017-11-14 22:05:28 +0200

    added a commentary just for lolz

--- app/src/main/java/com/anghelita/proiect_pso/MainActivity.java
+++ app/src/main/java/com/anghelita/proiect_pso/MainActivity.java
@@ -25,8 +25,7 @@ public class MainActivity extends AppCompatActivity {
     public void Register(View view) {
 
         Intent intent = new Intent(this, Register.class);
-
-        startActivity(intent);
+        startActivity(intent); //this comment should appear
 
     }
 
